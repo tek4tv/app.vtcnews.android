fdb15f1650852974eb2d996eed8a467c7de29eff
nts0311 <61538893+nts0311@users.noreply.github.com>
2020-12-31 08:45:55 +0700

    sửa nav drawer main

--- VTCNews/app/src/main/java/app/vtcnews/android/MainActivity.kt
+++ VTCNews/app/src/main/java/app/vtcnews/android/MainActivity.kt
@@ -1,10 +1,14 @@
 package app.vtcnews.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
+import android.os.PersistableBundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import app.vtcnews.android.databinding.ActivityMainBinding
 import app.vtcnews.android.ui.audio.AudioHomeFragment
 import app.vtcnews.android.ui.trang_chu.TrangChuFragment
@@ -14,6 +18,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+    private var curFrag = "trang_chu"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,26 +33,100 @@ class MainActivity : AppCompatActivity() {
             .commit()
 
         binding.mainBottomNav.setOnNavigationItemSelectedListener {
-            val audioFragment = AudioHomeFragment.newInstance()
-            if (it.itemId == R.id.menuAudio) {
-                if (audioFragment != null) {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_holder, audioFragment).addToBackStack(null).commit()
+            when (it.itemId) {
+                R.id.menuTrangChu -> {
+                    switchToTrangChu()
                 }
-            }
-            else if (it.itemId == R.id.menuTrangChu) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_holder, TrangChuFragment.newInstance())
-                    .addToBackStack(null).commit()
-            }
-            else if(it.itemId == R.id.menuTrending)
-            {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_holder, ArticlesFragment.newInstance(-1))
-                    .addToBackStack(null).commit()
+
+                R.id.menuTrending -> {
+                    switchToTrending()
+                }
+
+                R.id.menuAudio -> {
+                    switchToAudio()
+                }
             }
             true
         }
+
+        setupNavDrawer()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    private fun setupNavDrawer() {
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.root,
+            binding.toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        binding.root.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbar.setNavigationIcon(R.drawable.ic_nav_drawer_24)
+
+        binding.mainNavDrawer.setNavigationItemSelectedListener {
+
+            when(it.itemId)
+            {
+                R.id.item_nav_drawer_trang_chu -> {
+                    switchToTrangChu()
+                    binding.mainBottomNav.selectedItemId = R.id.menuTrangChu
+                }
+
+                R.id.item_nav_drawer_trending -> {
+                    switchToTrending()
+                    binding.mainBottomNav.selectedItemId = R.id.menuTrending
+                }
+
+                R.id.item_nav_drawer_audio -> {
+                    switchToAudio()
+                    binding.mainBottomNav.selectedItemId = R.id.menuAudio
+                }
+            }
+
+            binding.root.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+
+    private fun switchToTrangChu() {
+        if (curFrag == "trang_chu") return
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_holder, TrangChuFragment.newInstance())
+            .addToBackStack(null).commit()
+        curFrag = "trang_chu"
+    }
+
+    private fun switchToTrending() {
+        if (curFrag == "trending") return
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_holder, ArticlesFragment.newInstance(-1))
+            .addToBackStack(null).commit()
+        curFrag = "trending"
+    }
+
+    private fun switchToAudio() {
+        if (curFrag == "audio") return
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_holder, AudioHomeFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
+        curFrag = "audio"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
